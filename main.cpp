dde1d5b859fa104cc270a2e02c7e1280db0cfb42
js-on <jakobs@js-on.de>
2022-01-27 18:10:21 +0100

    update main.cpp: Fix bug test related to exercises

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
-#include <iostream>
 #include "string.cpp"
-
-void print_string(string s) {
-    s.print();
-}
 
 int main(int argc, char const *argv[])
 {
     string test("test");
-    print_string(test);
     test.print();
     return 0;
 }
